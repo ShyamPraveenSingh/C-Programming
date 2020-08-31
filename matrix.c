415fc6633bafe9329d59e8f532d0bf32230e6a7e
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-08-31 20:46:25 +0530

    matrix

---
+++ matrix.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m, r[20], c[20];
+    printf("\nEnter the number of rows: ");
+    scanf("%d",&n);
+    printf("\nEnter the number of columns: ");
+    scanf("%d", &m);
+    if (n!=m){
+        printf("\nNumber of rows should be equal to columns");
+    }
+}
