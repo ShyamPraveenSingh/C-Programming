0ed177ba6f911094093fb159a77f275aa6fc964f
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-01 10:29:50 +0530

    updates

--- matrix.c
+++ matrix.c
@@ -2,14 +2,41 @@
 
 int main()
 {
-    int n, m, r[20], c[20];
+    int r, c, m1[10][10], m2[10][10], res[10][10];
     printf("\nEnter the number of rows: ");
-    scanf("%d",&n);
+    scanf("%d",&r);
     printf("\nEnter the number of columns: ");
-    scanf("%d", &m);
-    if (n!=m){
-        printf("\nNumber of rows should be equal to columns");
-   }
+    scanf("%d", &c);
 
-   return 0;
+        //Debugging
+        if(r!=c){
+            printf("\nError! Rows and Columns should be equal.");
+            
+            printf("\nEnter the number of rows: ");
+            scanf("%d", &r);
+            printf("\nEnter the number of columns: ");
+            scanf("%d", &c);
+        }
+
+    printf("\nEnter the elements of first array: ");
+    for (int i=0; i<r; i++){
+        for (int j=0; j<c; j++){
+            printf("\nEnter the element \(%d,%d): ", i,j);
+            scanf("%d",&m1[i][j]);
+        }
+    }
+
+    printf("\nEnter the elements of second array: ");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("\nEnter the element \(%d,%d): ", i, j);
+            scanf("%d", &m2[i][j]);
+        }
+    }
+
+    return 0;
 }
+
+   
