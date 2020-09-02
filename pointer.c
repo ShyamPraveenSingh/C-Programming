61795a7fe77c9e0a7cfb4993aceb0242a1802dd8
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-02 20:09:24 +0530

    new

---
+++ pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+   int n, fact;
+
+   printf("\nEnter the number: ");
+   scanf("%d",&n);
+   while (n>0){
+       fact = n * (n-1);
+       n--;
+   }
+   printf("\nThe factorial of %d is %d.\n",n, fact);
+   return 0;
+}
