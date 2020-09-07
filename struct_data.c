44f9e38946bcaf892c7afa6d4e76768c3a636b35
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-07 10:45:07 +0530

    new

---
+++ struct_data.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct  new
+{
+    int a;
+    char c[20];
+    float p;
+};
+
+int main()
+{
+    struct new e = {12, "shyam", 5443.321};
+    printf("\nDetails: ");
+    printf("\nNumber: %d", e.a);
+    printf("\nCharacter: %s", e.c);
+    printf("\nFloat: %f", e.p);
+    printf("\nNumber: %d\n", e.a);
+
+    return 0;
+}
---
+++ struct_data2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct new
+{
+    int n;
+    char name[20];
+    float new;
+};
+
+
+int main()
+{
+    struct new a;
+    scanf("%d",&a.n);
+    scanf("%s",&a.name);
+    scanf("%f",&a.new);
+    printf("\nNumber: %d", a.n);
+    printf("\nName: %s", a.name);
+    printf("\nNew: %f\n", a.new);
+
+    return 0;
+}
