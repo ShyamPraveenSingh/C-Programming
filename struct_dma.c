11b9b3386d2f28d95736d022d737c86dc24433ac
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-08 15:18:08 +0530

    new

---
+++ struct_dma.c
@@ -0,0 +1,25 @@
+//Dynamic Memory Allocation of Structures
+
+#include <stdio.h>
+#include <stdlib.h>
+struct emp
+{
+    int eno;
+    char ename[20];
+    float esal;
+};
+
+void main()
+{
+    struct emp* ptr;
+
+    ptr = (struct emp*)malloc(sizeof(struct emp));
+    if (ptr==NULL){
+        printf("\n Out of memory.");
+    }
+    else{
+        printf("\n Enter employee details: ");
+        scanf("%d %s %f",&ptr->eno, ptr->ename, &ptr->esal);
+        printf("Details : \n %d %s %f \n", ptr->eno, ptr->ename, ptr->esal);
+    }
+}
