3921913cad1d992c940474cd051ff0e9de769335
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-11 19:34:13 +0530

    new

--- multipli_matrix.c
+++ multipli_matrix.c
@@ -2,15 +2,15 @@
 #define ROW 3
 #define COL 3
 
-int main()
+main()
 {
-    int a[ROW][COL], b[ROW][COL]; //c[ROW][COL];
+    int a[ROW][COL], b[ROW][COL], c[ROW][COL];
 
     printf("\nEnter the elements of 1st array: \n");
     for (int i = 0; i<ROW; i++){
         for (int j = 0; j<COL; j++){
             printf("\nEnter the element \(%d,%d): ", i, j);
-            scanf("%d",a[i][j]);
+            scanf("%d",&a[i][j]);
         }
     }
 
@@ -19,11 +19,24 @@ int main()
         for (int j = 0; j < COL; j++)
         {
             printf("\nEnter the element \(%d,%d): ", i, j);
-            scanf("%d", b[i][j]);
+            scanf("%d", &b[i][j]);
         }
     }
 
+    for(int i = 0; i<ROW; i++){
+        for (int j = 0; j<COL; j++){
+            c[i][j] = 0;
+            for(int k = 0; k<COL; k++){
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
 
-
-    return 0;
+    printf("\nThe resultant matrix is: \n");
+    for (int i = 0; i<ROW; i++){
+        for (int j = 0; j<COL; j++){
+            printf("\n%d",c[i][j]);
+        }
+    }
+    printf("\n");
 }
