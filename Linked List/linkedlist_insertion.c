310c8a0883e730672221525329e982d70f4799dc
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-18 09:40:09 +0530

    new

--- Linked List/linkedlist_insertion.c
+++ Linked List/linkedlist_insertion.c
@@ -11,4 +11,18 @@ struct node *addtobeg(struct node *start, int data)
     return start;
 }//End of addatbeg
 
-    
+// 2. At the end
+struct node *addtoend(struct node *start, int data)
+{
+    struct node *tmp, *p;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    p = start;
+    while (p->link!=NULL){}
+    {
+       p = p->link;
+    }
+    p->link = tmp;
+    tmp->link = NULL;
+    return start;
+} //End of addatend
