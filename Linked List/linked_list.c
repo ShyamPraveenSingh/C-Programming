81256a167eb1d4f02968352e018831e3951b375c
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-20 10:01:30 +0530

    new

--- Linked List/linked_list.c
+++ Linked List/linked_list.c
@@ -16,9 +16,55 @@ struct node *addatend(struct node *start, int data);
 main()
 {
     struct node *start = NULL;
-    int choice, data, item, pos;
-   
-    }
+
+    start = create_list(start);
+    
+
 }
 
+//Creation of a list
+struct node *create_list(struct node *start)
+{
+    int i, n, data;
+    printf("\nEnter the number of nodes: ");
+    scanf("%d", &n);
+    start = NULL;
+    if(n==0){
+        return start;
+    }
+    printf("\nEnter the element to be inserted: ");
+    scanf("%d",&data);
+    start = addatbeg(start, data);
+    for (i = 2; i<n; i++){
+        printf("\nEnter the element to be inserted: ");
+        scanf("%d", &data);
+        start = addatend(start, data);
+    }
+    return start;
+}
+
+//Insertion at the beginning
+struct node *addatbeg(struct node *start, int data)
+{
+    struct node *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    tmp->link = start;
+    start = tmp;
+    return start;
+}
+
+//Insertion at the end
+struct node *addatend(struct node *start, int data)
+{
+    struct node *p, *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    p = start;
+    while(p->link!=NULL)
+        p = p->link;
+    p->link = tmp;
+    tmp->link = NULL;
+    return start;
+}
 
