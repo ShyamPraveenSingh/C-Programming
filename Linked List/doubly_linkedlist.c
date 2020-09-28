4c7d912e15eb61e57e427e2832aca6123e0416db
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-09-28 14:00:47 +0530

    new

--- Linked List/doubly_linkedlist.c
+++ Linked List/doubly_linkedlist.c
@@ -10,18 +10,23 @@ struct node
     struct node *next;
 };
 
-struct node *display(struct node *start);
+void display(struct node *start);
+struct node *addtoempty(struct node *start, int data);
 struct node *addtobeg(struct node *start, int data);
 struct node *addtoend(struct node *start, int data);
-
+struct node *addafter(struct node *start);
+struct node *addbefore(struct node *start);
+struct node *create_list(struct node *start);
 
 main()
 {
-    struct node *start;
+    struct node *start=NULL;
+    start = create_list(start);
+    display(start);
 }
 
 //Displaying the elements 
-struct node *display(struct node *start)
+void display(struct node *start)
 {
     struct node *p;
     if(start == NULL){
@@ -39,6 +44,18 @@ struct node *display(struct node *start)
     printf("\n\n");
 }
 
+//Adding to the empty list
+struct node *addtoempty(struct node *start, int data)
+{
+    struct node *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    tmp->prev = NULL;
+    tmp->next = NULL;
+    start = tmp;
+    return start;
+}
+
 //Adding to the beginning of the list
 struct node *addtobeg(struct node *start, int data)
 {
@@ -52,6 +69,7 @@ struct node *addtobeg(struct node *start, int data)
     return start;
 }
 
+//Adding to the end
 struct node *addtoend(struct node *start, int data)
 {
     struct node *p, *tmp;
@@ -66,6 +84,70 @@ struct node *addtoend(struct node *start, int data)
     p->next = tmp;
     tmp->next = NULL;
     tmp->prev = p;
+    return start;  
+}
+
+//Adding after the given node
+struct node *addafter(struct node *start)
+{
+    int data, item;
+    printf("\nEnter the element to be inserted: ");
+    scanf("%d", &data);
+    printf("\nEnter the element after which to insert: ");
+    scanf("%d", &item);
+
+    struct node *p, *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    p = start;
+    while(p!=NULL)
+    {
+        if(p->info == item)
+        {
+            tmp->prev = p;
+            tmp->next = p->next;
+            if (p->next!=NULL)
+                p->next->prev = tmp;
+            p->next = tmp;
+            return start;
+        }
+       p = p->next;
+    }
+    printf("\n%d not present in the list\n", item);
     return start;
+}
     
+//Adding before the given node
+struct node *addbefore(struct node *start)
+{
+    int data, item;
+    printf("\nEnter the element to be inserted: ");
+    scanf("%d", &data);
+    printf("\nEnter the element after which to insert: ");
+    scanf("%d", &item);
+
+    //Just change p to q
+}
+
+
+//Creating linked list
+struct node *create_list(struct node *start)
+{
+    int i, n, data;
+    printf("\nEnter the number of nodes: ");
+    scanf("%d", &n);
+    start = NULL;
+    if (n == 0)
+        return start;
+    printf("\nEnter the element: ");
+    scanf("%d", &data);
+    start = addtoempty(start, data);
+
+    for (i = 2; i<=n; i++)
+    {
+        printf("\nEnter the element: ");
+        scanf("%d", &data);
+        start = addtoend(start, data);
+    }
+    return start;
 }
