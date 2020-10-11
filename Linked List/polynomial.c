ae0bbbf24e767f50aeadd1e6e729ea22d4ef6383
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-10-11 19:05:47 +0530

    new

--- Linked List/polynomial.c
+++ Linked List/polynomial.c
@@ -17,3 +17,38 @@ void display (struct node *ptr);
 void poly_add(struct node *, struct node *);
 void poly_mult(struct node *, struct node *);
 
+main()
+{
+    struct node *start1 = NULL, *start2 = NULL;
+    printf("\nEnter the 1st polynomial: ");
+    start1 = create(start1);
+    printf("\nEnter the 2nd polynomial: ");
+    start2 = create(start2);
+    printf("\nPolynomial 1 is: ");
+    display(start1);
+    printf("\nPolynomial 2 is: ");
+    display(start2);
+    poly_add(start1, start2);
+    poly_mult(start1, start2);
+}
+
+
+//Creation of the list
+struct node *create(struct node *start)
+{
+    int i, n, ex;
+    float co;
+    printf("\nEnter the number of terms: ");
+    scanf("%d", &n);
+    for (i = 0; i<n; i++)
+    {
+        printf("\nEnter the coefficient for term %d: ", i);
+        scanf("%f", &co);
+        printf("\nEnter the exponent for term %d: ", i);
+        scanf("%d", &ex);
+        start = insert_s(start, co, ex);
+    }
+    return start;
+}
+
+
