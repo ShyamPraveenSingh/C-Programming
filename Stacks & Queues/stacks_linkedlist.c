f2be212e79a39f7c2172032e00720c147e1443d7
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-10-16 22:08:34 +0530

    new

--- Stacks & Queues/stacks_linkedlist.c
+++ Stacks & Queues/stacks_linkedlist.c
@@ -14,3 +14,52 @@ int pop();
 int peek();
 int isEmpty();
 void display();
+
+main()
+{
+
+}
+
+//Push function to add to list
+void push(int item)
+{
+    struct node *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    if(tmp==NULL)
+    {
+        printf("\nStack Overflow");
+        return;
+    }
+    tmp->info = item;
+    tmp->link = top;
+    top = tmp;
+}
+
+//Pop function to remove the item from the list
+int pop()
+{
+    struct node *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    if (isEmpty())
+    {
+        printf("\nStacks Underflow");
+        exit(1);
+    }
+    tmp = top;
+    item = tmp->info;
+    top = top->link;
+    free(tmp);
+    return item;
+}
+
+//Peek function to look at the top of the list
+int peek()
+{
+    if(isEmpty())
+    {
+        printf("\nStack Underflow");
+        exit(1);
+    }
+    return top->info;
+}
+
