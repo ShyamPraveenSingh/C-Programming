5191d4a2aba37394d0662d83af664c7a9e1570d0
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-10-30 09:02:30 +0530

    new

---
+++ Recursion/tower_of_hanoi.c
@@ -0,0 +1,26 @@
+//Program to solve Tower of Hanoi using recursion
+
+#include<stdio.h>
+void tofh(int ndisk, char source, char temp, char dest);
+
+main()
+{
+    char source = 'A', temp = 'B', dest = 'C';
+    int ndisk;
+    printf("\nEnter the number of disks: ");
+    scanf("%d",&ndisk);
+    printf("\nSequence is: \n");
+    tofh(ndisk, source, temp, dest);
+}
+
+void tofh (int ndisk, char source, char temp, char dest)
+{
+    if(ndisk == 1)
+    {
+        printf("\nMove Disk %d from %c-->%c\n", ndisk, source, dest);
+        return;
+    }
+    tofh(ndisk-1, source, temp, dest);
+    printf("\nMove Disk %d from %c-->%c\n", ndisk, source, dest);
+    tofh(ndisk - 1, temp, source, dest);
+}
