c063ff1fc37289510d94b9e4012ed595291046e5
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-12-08 21:14:30 +0530

    graph

---
+++ graph_adjacency matrix.c
@@ -0,0 +1,16 @@
+//Program for creation of adjacency matrix
+
+#include <stdio.h>
+#define MAX 100
+
+int adj[MAX] [MAX];
+int n;
+
+main()
+{
+    int max_edge, i, j, origin, destin;
+    int graph_type;
+    printf("\nEnter 1 for undirected graph or 2 for directed graph: ");
+    scanf("%d", &n);
+    
+}
